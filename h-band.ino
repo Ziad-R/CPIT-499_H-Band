2be55222d5fe498dd50740485076457c3504074a
Ziad-R <43554884+Ziad-R@users.noreply.github.com>
2019-03-19 09:32:14 +0300

    Send data to cloud

--- h-band.ino
+++ h-band.ino
@@ -1,6 +1,17 @@
 
 #define USE_ARDUINO_INTERRUPTS true    // Set-up low-level interrupts for most acurate BPM math.
 #include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
+#include <SoftwareSerial.h>
+#define RX 10
+#define TX 11
+
+//WiFi Variables
+String AP = "Android-AP";
+String PASS = "70707070";
+String API = "2X2K8L7Y8T4FI1ND";
+String field1 = "pulse";
+String field2 = "temp";
+SoftwareSerial esp8266(RX,TX);
 
 //  Pulse Variables
 const int PulsePin = 0;
@@ -14,7 +25,11 @@ PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPla
 
 
 void setup() {   
-
+  esp8266.begin(115200);
+  sendCommand("AT",5,"OK");
+  sendCommand("AT+CWMODE=1",5,"OK");
+  sendCommand("AT+CWJAP=\""+ AP +"\",\""+ PASS +"\"",20,"OK");
+  
   Serial.begin(9600);          // For Serial Monitor
 
   // Configure the PulseSensor object, by assigning our variables to it. 
@@ -29,22 +44,31 @@ void setup() {
 
 
 void loop() {
- // Display Temp
+ // Send Temp
  value = analogRead(tempPin);
  float milliVolt = ( value/1024.0)*5000; 
  float tempC = milliVolt/10;
  Serial.print("TEMPRATURE = ");
  Serial.println(tempC);
-
- // Display Pulse
+  
+ String getData = "GET /update?api_key="+ API +"&"+ field2 +"="+String(value);
+ sendCommand("AT+CIPMUX=1",5,"OK");
+ sendCommand("AT+CIPSTART=0,\"TCP\",\""+ HOST +"\","+ PORT,15,"OK");
+ sendCommand("AT+CIPSEND=0," +String(getData.length()+4),4,">");
+ esp8266.println(getData);
+  
+ // Send Pulse
  int myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
 
   if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened". 
 
-  Serial.print("BPM: "); 
-  Serial.println(myBPM);                        // Print the value inside of myBPM. 
+  getData = "GET /update?api_key="+ API +"&"+ field1 +"="+String(value);
+  sendCommand("AT+CIPSEND=0," +String(getData.length()+4),4,">");
+  esp8266.println(getData);
+    
   }
-
+  
+  sendCommand("AT+CIPCLOSE=0",5,"OK");
   delay(1000);
 
 }
